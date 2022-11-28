3ffac73a2aef5136ac7c98e722e41d67792d6707
Pedro Papini <ppapinia@gmail.com>
2022-11-28 10:58:28 -0300

    Cria classe de validações para o login

--- app/backend/src/services/LoginService.ts
+++ app/backend/src/services/LoginService.ts
@@ -1,24 +1,11 @@
-import * as bcrypt from 'bcryptjs';
-import UserModel from '../database/models/UserModel';
-import { ILogin, IUser } from '../interfaces';
+import { ILogin } from '../interfaces';
 import generateJWT from '../utils/generateJWT';
+import LoginValidation from '../validations/LoginValidation';
 
 export default class LoginService {
-  private static async validateLoginData(loginData: ILogin): Promise<IUser> {
-    const { email, password } = loginData;
-    if (!email || !password) throw new Error('All fields must be filled');
-
-    const user = await UserModel.findOne({ where: { email } });
-    if (!user) throw new Error('Incorrect email or password');
-
-    const validPassword = bcrypt.compareSync(password, user.password);
-    if (!validPassword) throw new Error('Incorrect email or password');
-
-    return user;
-  }
-
   public static async doLogin(loginData: ILogin): Promise<string> {
-    const user = await LoginService.validateLoginData(loginData);
+    LoginValidation.validateLoginInputs(loginData);
+    const user = await LoginValidation.validateLoginData(loginData);
     const token = generateJWT(user);
     return token;
   }
---
+++ app/backend/src/validations/LoginValidation.ts
@@ -0,0 +1,21 @@
+import * as bcrypt from 'bcryptjs';
+import UserModel from '../database/models/UserModel';
+import { ILogin, IUser } from '../interfaces';
+
+export default class LoginValidation {
+  static validateLoginInputs(loginData: ILogin): void {
+    const { email, password } = loginData;
+    if (!email || !password) throw new Error('All fields must be filled');
+  }
+
+  static async validateLoginData(loginData: ILogin): Promise<IUser> {
+    const { email, password } = loginData;
+    const user = await UserModel.findOne({ where: { email } });
+    if (!user) throw new Error('Incorrect email or password');
+
+    const validPassword = bcrypt.compareSync(password, user.password);
+    if (!validPassword) throw new Error('Incorrect email or password');
+
+    return user;
+  }
+}
